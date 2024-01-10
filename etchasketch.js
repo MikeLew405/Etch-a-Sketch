2e6062ae676b8dbd22c97023fa8102881ce364ad
Mike Lewis <59457355+MikeLew405@users.noreply.github.com>
2024-01-09 19:14:35 -0500

    draft - attempt to use flex wrap

--- etchasketch.js
+++ etchasketch.js
@@ -1,4 +1,4 @@
-const GRID_COUNT = 16;
+const GRID_COUNT = 4;
 
 const body = document.body;
 const btn = document.querySelector('button');
@@ -12,14 +12,29 @@ btn.addEventListener('click', () => {
         generateGrid(input);
 });
 
+// function generateGrid(input = GRID_COUNT) {
+//     for (let i = 0; i < input; i++) {
+//         var row = document.createElement("div");
+//         row.classList.add('row');
+//         for (let j = 0; j < input; j++) {
+//             var column = document.createElement("div");
+//             column.classList.add('column');
+//             row.appendChild(column);
+//         }
+//         container.appendChild(row);
+//     }
+//     addListenerToGrid();    
+// }
+
 function generateGrid(input = GRID_COUNT) {
-    for (let i = 0; i < input; i++) {
-        var row = document.createElement("div");
-        for (let j = 0; j < input; j++) {
-            var column = document.createElement("div");
-            row.appendChild(column);
-        }
-        container.appendChild(row);
+    for (let i = 0; i < input * input; i++) {
+        var square = document.createElement("div");
+        square.classList.add('square');
+
+        let containerWidth = container.offsetWidth;
+        square.setAttribute("style", `width: ${containerWidth / input}px; height: ${container.offsetHeight / input}`);
+
+        container.appendChild(square);
     }
     addListenerToGrid();    
 }
